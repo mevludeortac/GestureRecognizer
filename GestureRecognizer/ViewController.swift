7e7fea703fc74f71e36a4395c29a520584d71da1
Mevlüde Saniye ORTAÇ <mevludeortac@users.noreply.github.com>
2021-05-31 18:49:37 +0300

    Initial Commit

---
+++ GestureRecognizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GestureRecognizer
+//
+//  Created by MEWO on 31.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
