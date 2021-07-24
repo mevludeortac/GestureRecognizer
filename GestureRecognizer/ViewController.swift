342547d6541866964dd209c08850693c01c92c55
mevludeortac <ortacmevlude@gmail.com>
2021-07-24 20:21:25 +0300

    view controller

--- GestureRecognizer/ViewController.swift
+++ GestureRecognizer/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var isKhont = true
+    @IBOutlet weak var chngLabel: UILabel!
+    @IBOutlet weak var img: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        img.isUserInteractionEnabled = true
+        let firstGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changePic))
+        img.addGestureRecognizer(firstGestureRecognizer)
     }
-
+    @objc func changePic(){
+        if isKhont == true{
+            img.image = UIImage(named: "khont2")
+            chngLabel.text =  "Gülen Khont"
+            isKhont=false
+        }else{
+            img.image = UIImage(named: "khont")
+            chngLabel.text = "Sinirli Khont"
+            isKhont=true
+        }
+    }
 
 }
 
